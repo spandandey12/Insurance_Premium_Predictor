57151b5aa4847b204ace1aaa8611b209af74df4a
spandandey12 <spandandey021@gmail.com>
2025-12-04 08:06:23 -0800

    Added one ts file

---
+++ tests/testHelper.py
@@ -0,0 +1,27 @@
+import unittest
+import pandas as pd
+
+class TestHelper(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        print("Started TestHelper.")
+    @classmethod
+    def tearDownClass(cls):
+        print("Finished TestHelper.")
+    def setUp(self):
+        self.df = pd.DataFrame({"a":[1,2,3]})
+    def tearDown(self):
+        pass
+    def test_dataframe(self):
+        self.assertEqual(len(self.df),3)
+        self.assertIn("a",self.df.columns)
+        self.assertEqual(self.df["a"].sum(), 6)
+        self.assertIsInstance(self.df.iloc[0]["a"], int)
+    def test_math(self):
+        x,y=4,5
+        self.assertEqual(x + y, 9)
+        self.assertNotEqual(x * y, 10)
+        self.assertGreater(y, x)
+        self.assertIsInstance(x, int)
+if __name__ == "__main__":
+    unittest.main()
